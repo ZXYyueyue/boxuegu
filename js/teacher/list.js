d57d196250e980d7880860dbcca88c7b0cb4ca51
guopf <626138959@qq.com>
2017-03-01 10:36:01 +0800

    讲师状态修改

--- js/teacher/list.js
+++ js/teacher/list.js
@@ -23,4 +23,24 @@ define(['jquery', 'common', 'nprogress', 'template'], function($, undefined, npr
 			}
 		});
 	});
+	
+	// 讲师状态修改
+	$('#teacher-list-tbody').on('click', '.teacher-status', function() {
+		var $self = $(this);
+		$.ajax({
+			url: '/v6/teacher/handle',
+			type: 'post',
+			data: {
+				tc_id: $(this).parent().attr('data-id'),
+				tc_status: $(this).parent().attr('data-status')
+			},
+			success: function(data) {
+				if(data.code == 200) {
+					// 得到修改后的状态，使用该状态修改按钮名称&父元素的data-status属性值
+					$self.html(data.result.tc_status == 0? '开启': '注销');
+					$self.parent().attr('data-status', data.result.tc_status);
+				}
+			}
+		});
+	});
 });
